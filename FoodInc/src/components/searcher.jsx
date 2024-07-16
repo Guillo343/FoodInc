7cb92d13f1a95bd3522f8bdcfccdd5baa810c9cf
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-07-16 17:38:14 -0500

    Summary

--- FoodInc/src/components/searcher.jsx
+++ FoodInc/src/components/searcher.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import '../style/searcher.css'
+import video1 from '../assets/video1.mp4'
+import video2 from '../assets/video2.mp4'
+import video3 from '../assets/video3.mp4'
+import video4 from '../assets/video4.mp4'
 
 export const Searcher = () => {
+    const videos = [video1, video2, video3, video4]  
+
   return (
     <>
       <div className="video-background">
-        <video src="../assets/video1.mp4" autoPlay loop muted />
-        <video src="../assets/video2.mp4" autoPlay loop muted />
-        <video src="../assets/video3.mp4" autoPlay loop muted />
-        <video src="../assets/video4.mp4" autoPlay loop muted />
+        {videos.map((video, index) => (
+          <video key={index} src={video} autoPlay loop muted />
+        ))}
         <div className="overlay">
           <div className="welcome-text">
             <h1>Welcome to FoodInc</h1>
@@ -19,5 +24,6 @@ export const Searcher = () => {
           </div>
         </div>
       </div>
-    </>  )
-}
+    </>
+    );
+};
