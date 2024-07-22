5113d380efc323b5f8cc8122dfbc8a0e09d96922
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-07-22 18:08:22 -0500

    Summary

--- FoodInc/src/App.jsx
+++ FoodInc/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Navbar } from './components/navbar'
 import { Searcher } from './components/searcher'
+import { Api } from './components/api'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
        <Navbar />
        <Searcher />
+       <Api />
     </>
   )
 }
--- FoodInc/src/components/api.jsx
+++ FoodInc/src/components/api.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import '../style/info.css'
 
-export const api = () => {
+export const Api = () => {
   return (
-    <div>api</div>
+    <div className="recipe-container">
+    <div className="recipe-header">
+      <h1>Dish</h1>
+    </div>
+    <div className="recipe-content">
+      <div className="ingredients">
+        <h2>Ingredientes</h2>
+        <ul>
+          <li>Papas</li>
+          <li>Agua</li>
+        </ul>
+      </div>
+      <div className="steps">
+        <h2>Steps</h2>
+        <ol>
+          <li>Hervir Agua</li>
+          <li>Sacar papas</li>
+          <li>Pelarlas</li>
+          <li>Ponerlas a hervir</li>
+          <li>Sacar luego de 7min</li>
+        </ol>
+      </div>
+      <div className="images">
+        <img src="path/to/orange-juice.png" alt="Orange Juice" className="food-image" />
+        <img src="path/to/sandwich.png" alt="Sandwich" className="food-image" />
+      </div>
+    </div>
+    <div className="photos">
+      <h2>Photos</h2>
+      {/* Add photos here */}
+    </div>
+  </div>
   )
 }
