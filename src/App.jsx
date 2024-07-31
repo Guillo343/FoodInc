c15f0ec7481c321d6a7461e349a71e6aabb5b6ee
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-07-30 22:49:44 -0500

    Summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import React from "react";
 import "./App.css";
 import { Navbar } from "./components/navbar";
 import { Searcher } from "./components/searcher";
 import { Info } from "./components/info";
 import { RecipeSearch } from "./components/recipeSearch";
 import { InputSearcher } from "./components/InputSearcher";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Searcher />} />
+        <Route path="/" element={<><Searcher />
+                                  <InputSearcher />
+                                  </>} />
         <Route path="/info" element={<Info />} />
         <Route path="/input-searcher" element={<InputSearcher />} />
         <Route path="/recipe-search" element={<RecipeSearch />} />
