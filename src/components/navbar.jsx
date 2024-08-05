88906bf1b1bc80cac18eeda79a819509f3d20b7f
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-08-04 22:02:10 -0500

    Summary

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/navbar.css';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <a href="/"><img src="/logoAlt.png" alt="Logo" /> FoodInc</a>
       </div>
-      <ul className="navbar-menu">
-        <li><Link className="underlineLink" href="/search">Search</Link></li>
-        <li><Link className="underlineLink" href="/recipes">Recipes</Link></li>
-        <li><Link className="underlineLink" href="/about">About</Link></li>
-        <li><Link className="underlineLink" href="/contact">Contact Us</Link></li>
+      <div className='navbar-toggle' onClick={toggleMenu}>&#9776;</div>
+      <ul className={`navbar-menu ${menuOpen ? 'active' : ''}`}>
+        <li><Link className="underlineLink" to='/search' onClick={toggleMenu}>Search</Link></li>
+        <li><Link className="underlineLink" to='/recipes' onClick={toggleMenu}>Recipes</Link></li>
+        <li><Link className="underlineLink" to='/about' onClick={toggleMenu}>About</Link></li>
+        <li><Link className="underlineLink" to='/contact' onClick={toggleMenu}>Contact Us</Link></li>
       </ul>
     </nav>
   );
